62bd8e6c99ca20d6ba32ed30006070366223c9b7
Manjot Sidhu <manjot.gni@gmail.com>
2017-09-12 13:16:29 +0530

    Revert "MediaTek: add SoftVideoDecoder blobs." Causing Build Error This reverts commit ba7b961e4d51a3de3b7d61fe3aadcc2e0ec5c063.

--- media/libstagefright/include/SoftVideoDecoderOMXComponent.h
+++ media/libstagefright/include/SoftVideoDecoderOMXComponent.h
@@ -97,9 +97,6 @@ protected:
             const char *mimeType,
             OMX_U32 minCompressionRatio = 1u);
 
-    // For MTK blob compatibility
-    virtual void updatePortDefinitions(bool updateCrop = true);
-
     virtual void updatePortDefinitions(bool updateCrop = true, bool updateInputSize = false);
 
     uint32_t outputBufferWidth();
